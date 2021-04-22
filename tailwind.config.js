e4394ba88683765a801288ada785f4bd86730868
AryaTandon <atandon23@outlook.com>
2021-04-22 17:21:03 +0100

    Minor edit to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
+      black: colors.black,
       red: colors.red,
       yellow: colors.yellow,
       green: colors.green,
